d7eb6518083ca3263e1191269471b63766990644
chahuihou <865556380@qq.com>
2024-10-16 11:03:48 +0800

    save

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	}
 }
 
-void Do_Movement()
+static void Do_Movement()
 {
 	// Camera controls
 	if (keys[GLFW_KEY_W])
@@ -51,7 +51,7 @@ void Do_Movement()
 		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
 	if (button == GLFW_MOUSE_BUTTON_LEFT) {
 		leftButtonPressed = (action == GLFW_PRESS);
 		if (leftButtonPressed) {
@@ -76,7 +76,7 @@ static void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 			}
 
 		GLfloat xoffset = xpos - lastX;
-		GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+		GLfloat yoffset = ypos - lastY;
 
 		lastX = xpos;
 		lastY = ypos;
@@ -90,6 +90,268 @@ static void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 	camera.ProcessMouseScroll(yoffset);
 }
 
+void camera_bind(Shader ourShader)
+{
+	glm::mat4 view;
+	view = camera.GetViewMatrix();
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(camera.Zoom), (float)SreenWidth / (float)SreenHeight, 0.1f, 100.0f);
+	// Get the uniform locations
+	//GLint modelLoc = glGetUniformLocation(outShader.Program, "model");
+	GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+	GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
+	// Pass the matrices to the shader
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+//三角顶点
+GLfloat trangle_One[] = {
+	 //Positions         // Colors
+	0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // Bottom Right
+   -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // Bottom Left
+	0.0f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f   // Top 
+};
+GLfloat trangle_Two[] = {
+	 //Positions         // Colors
+	0.8f, -0.5f, 0.0f,   0.0f, 1.0f, 1.0f,  // Bottom Right
+   -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 1.0f,  // Bottom Left
+	0.0f,  0.5f, 0.0f,   0.0f, 1.0f, 1.0f   // Top 
+};
+GLfloat trangle_Three[] = {
+	 //Positions         // Colors
+	1.0f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Right
+   -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+	0.0f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f   // Top 
+};
+
+//数据自带一个float偏移的三角顶点
+//GLfloat trangle_One[] = {
+//	// Positions         // Colors
+//	0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // Bottom Right
+//   -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  // Bottom Left
+//	0.0f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f   // Top 
+//};
+//GLfloat trangle_Two[] = {
+//	// Positions         // Colors
+//	0.5f, -0.5f, 0.000001f,   0.0f, 1.0f, 1.0f,  // Bottom Right
+//   -0.5f, -0.5f, 0.000001f,   0.0f, 1.0f, 1.0f,  // Bottom Left
+//	0.0f,  0.5f, 0.000001f,   0.0f, 1.0f, 1.0f   // Top 
+//};
+//GLfloat trangle_Three[] = {
+//	// Positions         // Colors
+//	0.5f, -0.5f, 0.000001f,   0.0f, 1.0f, 0.0f,  // Bottom Right
+//   -0.5f, -0.5f, 0.000002f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+//	0.0f,  0.5f, 0.000003f,   0.0f, 1.0f, 0.0f   // Top 
+//};
+
+//线段顶点
+GLfloat line_One[] = {
+	// Positions         // Colors
+	0.4f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
+   -0.4f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Left
+	0.0f,  0.4f, 0.0f,   1.0f, 0.0f, 0.0f   // Top 
+};
+GLfloat line_Two[] = {
+	// Positions         // Colors
+	0.7f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Right
+   -0.4f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
+	0.0f,  0.4f, 0.0f,   0.0f, 1.0f, 0.0f   // Top 
+};
+GLfloat line_Three[] = {
+	// Positions         // Colors
+	0.9f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  // Bottom Right
+   -0.4f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  // Bottom Left
+	0.0f,  0.4f, 0.0f,   0.0f, 0.0f, 1.0f   // Top 
+};
+
+unsigned int VAO[6], VBO[6];
+
+//按照黄色实体、蓝色实体、绿色实体、红色线框、绿色线框、蓝色线框顺序提交数据
+void data_bind_one()
+{
+	memset(VAO, 0, size(VAO));
+	memset(VBO, 0, size(VBO));
+	glGenVertexArrays(6, VAO);
+	glGenBuffers(6, VBO);
+
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_One), trangle_One, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_Two), trangle_Two, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[2]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_Three), trangle_Three, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[3]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_One), line_One, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[4]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[4]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_Two), line_Two, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(VAO[5]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[5]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_Three), line_Three, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+}
+
+//绿色实体、蓝色实体、黄色实体、绿色线框、红色线框、蓝色线框顺序提交数据
+void data_bind_two()
+{
+	memset(VAO, 0, size(VAO));
+	memset(VBO, 0, size(VBO));
+	glGenVertexArrays(6, VAO);
+	glGenBuffers(6, VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_Three), trangle_Three, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_Two), trangle_Two, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trangle_One), trangle_One, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[2]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[3]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_Two), line_Two, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[3]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[4]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_One), line_One, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[4]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[5]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(line_Three), line_Three, GL_STATIC_DRAW);
+	glBindVertexArray(VAO[5]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+}
+
+//按照黄色实体、蓝色实体、绿色实体、红色线框、绿色线框、蓝色线框顺序绘制
+void data_draw_one()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glPolygonOffset(2.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+
+	glBindVertexArray(VAO[0]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(4.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[1]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(6.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[2]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(2.0);
+
+	glPolygonOffset(1.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[3]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(3.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[4]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(5.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[5]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+//绿色实体、蓝色实体、黄色实体、绿色线框、红色线框、蓝色线框顺序绘制
+void data_draw_two()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glPolygonOffset(6.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[0]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(4.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[1]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(2.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[2]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glLineWidth(2.0);
+
+	glPolygonOffset(3.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[3]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(1.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[4]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glPolygonOffset(5.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
+	glBindVertexArray(VAO[5]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
 int main()
 {
 	glfwInit();//初始化
@@ -101,17 +363,17 @@ int main()
 
 	GLFWwindow* window = glfwCreateWindow(SreenWidth, SreenHeight, "OpenGL", nullptr, nullptr);
 	
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetMouseButtonCallback(window, mouse_button_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetKeyCallback(window, key_callback); //绑定键盘
+	glfwSetMouseButtonCallback(window, mouse_button_callback); //绑定鼠标按键
+	glfwSetCursorPosCallback(window, mouse_callback); //绑定鼠标移动
+	glfwSetScrollCallback(window, scroll_callback); //绑定鼠标滚轮
 
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
 	if (window == nullptr)
 	{
 		cout << "Failed to create GLFW window" << endl;
-		glfwTerminate();//terminate the glfw(end the glfw)
+		glfwTerminate(); //terminate the glfw(end the glfw)
 		return -1;
 	}
 
@@ -123,62 +385,17 @@ int main()
 		return -1;
 	}
 
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
-
-	glViewport(0, 0, SreenWidth, SreenHeight);
-	// 启用深度测试
-	glEnable(GL_DEPTH_TEST);
-	// 设置深度测试函数 GL_LESS GL_LEQUAL
-	glDepthFunc(GL_LESS);
-
 	Shader outShader("./shaders/trans-vert.vs", "./shaders/trans-frag.frag");
 
-	float value = 0.0;
+	glViewport(0, 0, SreenWidth, SreenHeight);
+	glEnable(GL_DEPTH_TEST);// 启用深度测试
+	glDepthFunc(GL_LESS);// 设置深度测试函数 GL_LESS GL_LEQUAL
 
-	GLuint VAO, VBO;
-
+	//data_bind_one();
+	data_bind_two();
 	while (!glfwWindowShouldClose(window))
 	{
-
-		// 开始新帧
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-		// 创建UI窗口
-		ImGui::Begin("Vertex Control");
-
-		// 添加用于修改顶点坐标
-		ImGui::InputFloat("Value", &value, 0.000001f, 0.1f, "%.8f");
-
-		ImGui::End();
-		// Rendering
-		ImGui::Render();
-
-		//三角顶点
-		GLfloat vertices[] = {
-			// Positions         // Colors
-			0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
-		   -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Left
-			0.0f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   // Top 
-
-			0.5f, -0.5f, value,   0.0f, 1.0f, 0.0f,  // Bottom Right
-		   -0.5f, -0.5f, value,   0.0f, 1.0f, 0.0f,  // Bottom Left
-			0.0f,  0.5f, value,   0.0f, 1.0f, 0.0f   // Top 
-		};
-		//vectices data
-		glGenVertexArrays(1, &VAO);//获得一个顶点数组对象
-		glBindVertexArray(VAO);
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-		glEnableVertexAttribArray(1);
-		glBindVertexArray(0);
-
+		glfwPollEvents();//检查有没有触发事件
 		GLfloat currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
@@ -188,43 +405,19 @@ int main()
 
 		outShader.Use();
 
-		glm::mat4 view;
-		view = camera.GetViewMatrix();
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)SreenWidth / (float)SreenHeight, 0.1f, 100.0f);
-		// Get the uniform locations
-		//GLint modelLoc = glGetUniformLocation(outShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(outShader.Program, "view");
-		GLint projLoc = glGetUniformLocation(outShader.Program, "projection");
-		// Pass the matrices to the shader
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		camera_bind(outShader);
 
-		glBindVertexArray(VAO);
+		glEnable(GL_POLYGON_OFFSET_FILL);// 启用多边形FILL偏移
+		glEnable(GL_POLYGON_OFFSET_LINE);// 启用多边形LINE偏移
 
-		glEnable(GL_POLYGON_OFFSET_FILL);
-		glPolygonOffset(1.0f, 1.0f); // 调整偏移值，具体值可以根据场景调整
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//data_draw_one();
+		data_draw_two();
 
-		// 恢复偏移状态
-		glDisable(GL_POLYGON_OFFSET_FILL);
-
-		glDrawArrays(GL_TRIANGLES, 3, 3);
-		glBindVertexArray(0);
-
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		glfwSwapBuffers(window);//双缓冲（替换窗口的每一个像素值）
-		glfwPollEvents();//检查有没有触发事件
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-
-	// 清理
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
+	glDeleteVertexArrays(6, VAO);
+	glDeleteBuffers(6, VBO);
 	glfwTerminate();
 	return 0;
 }
--- tool/cameraFile.h
+++ tool/cameraFile.h
@@ -7,6 +7,7 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 
 
@@ -22,7 +23,7 @@ enum Camera_Movement {
 const GLfloat YAW = -90.0f;
 const GLfloat PITCH = 0.0f;
 const GLfloat SPEED = 3.0f;
-const GLfloat SENSITIVTY = 0.25f;
+const GLfloat SENSITIVTY = 0.1f;
 const GLfloat ZOOM = 45.0f;
 
 
@@ -32,6 +33,7 @@ class Camera
 public:
     // Camera Attributes
     glm::vec3 Position;
+    glm::vec3 Target;
     glm::vec3 Front;
     glm::vec3 Up;
     glm::vec3 Right;
@@ -45,28 +47,37 @@ public:
     GLfloat Zoom;
 
     // Constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), GLfloat yaw = YAW, GLfloat pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), 
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), 
+           GLfloat yaw = YAW, 
+           GLfloat pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), 
+           MovementSpeed(SPEED), 
+           MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
     {
         this->Position = position;
         this->WorldUp = up;
+        this->Up = up;
         this->Yaw = yaw;
         this->Pitch = pitch;
-        this->updateCameraVectors();
+        GLfloat x = 0, y = 0;
+        this->updateCameraVectors(x, y);
     }
     // Constructor with scalar values
     Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY, GLfloat upZ, GLfloat yaw, GLfloat pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVTY), Zoom(ZOOM)
     {
         this->Position = glm::vec3(posX, posY, posZ);
         this->WorldUp = glm::vec3(upX, upY, upZ);
+        this->Up = glm::vec3(upX, upY, upZ);
         this->Yaw = yaw;
         this->Pitch = pitch;
-        this->updateCameraVectors();
+        //this->updateCameraVectors();
     }
 
     // Returns the view matrix calculated using Eular Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix()
     {
-        return glm::lookAt(this->Position, this->Position + this->Front, this->Up);
+        glm::vec3 target(0.0f, 0.0f, 0.0f);
+        return glm::lookAt(this->Position, target, this->Up);
     }
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -90,44 +101,43 @@ public:
         yoffset *= this->MouseSensitivity;
 
         this->Yaw += xoffset;
-        this->Pitch += yoffset;
-
-        // Make sure that when pitch is out of bounds, screen doesn't get flipped
-        if (constrainPitch)
-        {
-            if (this->Pitch > 89.0f)
-                this->Pitch = 89.0f;
-            if (this->Pitch < -89.0f)
-                this->Pitch = -89.0f;
-        }
+        this->Pitch -= yoffset;
 
         // Update Front, Right and Up Vectors using the updated Eular angles
-        this->updateCameraVectors();
+        this->updateCameraVectors(xoffset, yoffset);
     }
 
     // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
     void ProcessMouseScroll(GLfloat yoffset)
     {
-        if (this->Zoom >= 1.0f && this->Zoom <= 45.0f)
-            this->Zoom -= yoffset;
-        if (this->Zoom <= 1.0f)
-            this->Zoom = 1.0f;
-        if (this->Zoom >= 45.0f)
-            this->Zoom = 45.0f;
+        this->Zoom -= yoffset;
     }
 
 private:
     // Calculates the front vector from the Camera's (updated) Eular Angles
-    void updateCameraVectors()
+    void updateCameraVectors(GLfloat &xoffset, GLfloat &yoffset)
     {
-        // Calculate the new Front vector
-        glm::vec3 front;
-        front.x = cos(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
-        front.y = sin(glm::radians(this->Pitch));
-        front.z = sin(glm::radians(this->Yaw)) * cos(glm::radians(this->Pitch));
-        this->Front = glm::normalize(front);
-        // Also re-calculate the Right and Up vector
-        this->Right = glm::normalize(glm::cross(this->Front, this->WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        this->Up = glm::normalize(glm::cross(this->Right, this->Front));
+        //ÈÆµã£¨0.0£¬ 0.0£¬ 0.0£©Ðý×ª
+        glm::vec3 direction = this->Front - this->Position;
+        direction = glm::normalize(direction);
+        this->Right = glm::normalize(glm::cross(direction, this->Up));
+        
+        glm::quat aQuat{ 0, 0, 0, 1 };
+        aQuat = glm::angleAxis(yoffset * this->MouseSensitivity, this->Right);
+        aQuat = glm::angleAxis(xoffset * this->MouseSensitivity, this->Up) * aQuat;
+
+        auto func = [](glm::vec3 src, const glm::vec3& center, const glm::quat& aQuat)
+        {
+            src -= center;
+            src = src * glm::mat3_cast(aQuat);
+            src += center;
+            return src;
+        };
+        glm::vec3 center{ 0.0, 0.0, 0.0 };
+        this->Position = func(this->Position, center, aQuat);
+        this->Front = func(this->Front, center, aQuat);
+        glm::vec3 dir = glm::normalize(this->Position - this->Front);
+
+        this->Up = glm::normalize(glm::cross(dir, this->Right));
     }
 };
